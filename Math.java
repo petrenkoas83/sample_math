8adead25420c846ed30c35705d35067b7d51f679
Alexandr Petrenko <petrenkoas83@gmail.com>
2017-09-05 18:32:57 +0300

    C8. Reduce printf.

--- Math.java
+++ Math.java
@@ -9,16 +9,18 @@ public class Math {
 		Integer a = Integer.valueOf(args[0]);
 		String action = args[1];
 		Integer b = Integer.valueOf(args[2]);
+		Integer r;
 		switch (action) {
 			case "-":
-				System.out.printf("Result: %d\n", subtraction(a,b));
+				r = subtraction(a,b);
 				break;
 			case "+":
-				System.out.printf("Result: %d\n", addition(a,b));
+				r = addition(a,b);
 				break;
 			default:
 				System.out.printf("Unknown operator '%s'!\n", action);
 				break;
 		}
+		System.out.prinf("%d",r);
 	}
 }
